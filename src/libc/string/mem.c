088cc3dac18d1df4927a5758883e8e52bfa849c9
one to go <dda263071@gmail.com>
2026-02-11 17:45:43 +0700

    Create mem.c

---
+++ src/libc/string/mem.c
@@ -0,0 +1,37 @@
+#include <libc/string/string.h>
+
+void* memcpy(void* dest, const void* src, size_t n) {
+    uint8_t* d = dest;
+    const uint8_t* s = src;
+    while (n--) *d++ = *s++;
+    return dest;
+}
+
+void* memmove(void* dest, const void* src, size_t n) {
+    uint8_t* d = dest;
+    const uint8_t* s = src;
+    if (d < s) {
+        while (n--) *d++ = *s++;
+    } else {
+        d += n;
+        s += n;
+        while (n--) *--d = *--s;
+    }
+    return dest;
+}
+
+void* memset(void* s, int c, size_t n) {
+    uint8_t* p = s;
+    while (n--) *p++ = (uint8_t)c;
+    return s;
+}
+
+int memcmp(const void* s1, const void* s2, size_t n) {
+    const uint8_t* p1 = s1;
+    const uint8_t* p2 = s2;
+    while (n--) {
+        if (*p1 != *p2) return *p1 - *p2;
+        p1++; p2++;
+    }
+    return 0;
+}
