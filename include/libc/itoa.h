1de2ddca1478e55648e5bd5f9dcf38f3ac506758
one to go <dda263071@gmail.com>
2026-02-11 17:08:50 +0700

    Create itoa.h

---
+++ include/libc/itoa.h
@@ -0,0 +1,14 @@
+#ifndef ITOA_H
+#define ITOA_H
+
+#include "types.h"
+
+void itoa(int32_t value, char* str, int base);
+void utoa(uint32_t value, char* str, int base);
+void itoa64(int64_t value, char* str, int base);
+void utoa64(uint64_t value, char* str, int base);
+
+void reverse_string(char* str, int length);
+int  strlen(const char* str);
+
+#endif
