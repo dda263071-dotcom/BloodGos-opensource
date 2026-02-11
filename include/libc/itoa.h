6f4c9da2bde8690677705c1ba317e66df93b928e
one to go <dda263071@gmail.com>
2026-02-11 17:47:27 +0700

    Update itoa.h

--- include/libc/itoa.h
+++ include/libc/itoa.h
@@ -1,14 +1,13 @@
 #ifndef ITOA_H
 #define ITOA_H
 
-#include "types.h"
+#include <libc/types.h>
+#include <libc/string/string.h>   /* strlen now here */
 
 void itoa(int32_t value, char* str, int base);
 void utoa(uint32_t value, char* str, int base);
 void itoa64(int64_t value, char* str, int base);
 void utoa64(uint64_t value, char* str, int base);
-
 void reverse_string(char* str, int length);
-int  strlen(const char* str);
 
 #endif
