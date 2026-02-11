b5297e86ce457a21d5845ac152f20bd60dbbb45d
one to go <dda263071@gmail.com>
2026-02-11 17:05:59 +0700

    Update timer.h

--- include/libc/timer.h
+++ include/libc/timer.h
@@ -1,1 +1,14 @@
+#ifndef TIMER_H
+#define TIMER_H
 
+#include "types.h"
+
+void timer_init(uint32_t frequency_hz);
+void sleep_ms(uint32_t milliseconds);
+uint64_t get_ticks(void);
+void timer_interrupt_handler(void);
+
+#define TIMER_FREQ 1000
+#define TICKS_PER_MS 1
+
+#endif
